b196cb3167c23743bd7fcfe10462234a2fc3cdad
ThienNhi <thien-nhi.nguyen@smail.th-koeln.de>
2016-03-26 22:12:59 +0100

    SEO
    metatag im head, robots.txt (alles erlaubt)

--- partials/head.php
+++ partials/head.php
@@ -1,4 +1,4 @@
-<meta name="description" content="WPF">
+<meta name="description" content="Dies ist eine Final Fantasy Fanseite, die auf neue Releases zeitnah News hochlädt. Zudem können anhand der Gallerie neu veröffentlichte Bilder gesehen werden.">
 <meta name="author" content="Nguyen und Tennie">
 <link rel="stylesheet" href="css/stylesheet.css">
 <link rel="stylesheet" href="css/lightbox.min.css">
@@ -8,4 +8,4 @@
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 
-<title> Final Fantasy Fanseite </title>
+<title> Final Fantasy Fanseite</title>
