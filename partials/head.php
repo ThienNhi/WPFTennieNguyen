b0079ff1d6d43854ea02677f99cc96b63c96b812
ThienNhi <thien-nhi.nguyen@smail.th-koeln.de>
2016-03-26 22:03:03 +0100

    min js und css der lightbox

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,8 @@
 <meta name="description" content="WPF">
 <meta name="author" content="Nguyen und Tennie">
 <link rel="stylesheet" href="css/stylesheet.css">
-<link rel="stylesheet" href="css/lightbox.css">
-<script src="js/lightbox-plus-jquery.js"></script>
+<link rel="stylesheet" href="css/lightbox.min.css">
+<script src="js/lightbox-plus-jquery.min.js"></script>
 
 <!--Noch nirgends benutzt Bootstrap -->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
