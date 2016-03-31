5e824da60587f56efd60f8a8f508471b0474b7c3
ThienNhi <thien-nhi.nguyen@smail.th-koeln.de>
2016-03-31 22:28:59 +0200

    nochmal error page

---
+++ error.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+		<?php include 'partials/head.php';?>
+		<title>Error</title>
+	</head>
+
+	<body class="test">
+		<div class="content">
+<!-- Menü-->
+            <?php include 'partials/menue.php';?> 
+            <?php include 'partials/navbar.php';?>
+
+<!-- Versuch, um die Bilder herum Text zu schreiben-->
+			<div id="content">
+				<div id="inner">
+				<div id="text">
+					<p align="justify"> 
+						<h1>Error</h1> 
+							Irgendwas ist schief gelaufen!<br />
+							Sie d&uuml;rfen auf diese Seite nicht zugreifen.<br />
+							Wenn Sie wieder auf die Hauptseite wollen, klicken Sie <a href="index.php">hier</a><br />
+					</p>
+				</div>
+				</div>
+				<?php include 'partials/footer.php';?>	
+			</div>
+
+
+	</body>
+</html>
